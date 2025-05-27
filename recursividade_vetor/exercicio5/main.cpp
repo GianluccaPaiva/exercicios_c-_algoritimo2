f0958a1906cac67bf8143620f857e44fd8d1d606
Gianlucca Paiva <gianluccapaiva@gmail.com>
2025-05-26 23:08:09 -0300

    ...

--- recursividade_vetor/exercicio5/main.cpp
+++ recursividade_vetor/exercicio5/main.cpp
@@ -4,20 +4,22 @@ using namespace std;
 
 int preencheVet(int vet[], int n){
     if(n == 1){
-        vet [n] = 1;
+        vet [n-1] = 1;
+        return 1;
     }
     vet[n-1] = n*n+1;
-    preencheVet(vet, n-1);
+    return vet[n-1] + preencheVet(vet, n-1);
 }
 
 int main()
 {
     int n;
+    cout<<"tamanho do vetor: ";
     cin>>n;
     int *v = new int [n];
-    cout<<preencheVet(v, n);
+    cout<<"Soma: "<<preencheVet(v, n)<<endl;
     for(int i = 0; i<n; i++){
-        cout<< v[i]<<edl;
+        cout<< v[i]<<endl;
     }
     delete [] v;
     return 0;
